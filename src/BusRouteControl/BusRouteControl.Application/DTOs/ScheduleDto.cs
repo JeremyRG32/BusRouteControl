a1e24e822294895335c6550c98970a348c213b03
JeremyRG32 <Jeremyrg619@gmail.com>
2025-03-24 16:27:29 -0400

    Revert "Created Services"
    This reverts commit 4b21a85fa5d324c258869e8969f10cb4bc8517c9.

--- src/BusRouteControl/BusRouteControl.Application/DTOs/ScheduleDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BusRouteControl.Application.DTOs
-{
-    public class ScheduleDto
-    {
-        public int Id { get; set; }
-        public TimeSpan DepartureTime { get; set; }
-        public TimeSpan ArrivalTime { get; set; }
-    }
-}
